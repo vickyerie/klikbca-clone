a5dfd6b1c95e31f7514693b017f0cc0a11c1b35b
vickyerie <vickyerie.s@gmail.com>
2025-06-11 20:40:25 +0700

    insert user seed data

--- klikbca-clone/database/seeders/DatabaseSeeder.php
+++ klikbca-clone/database/seeders/DatabaseSeeder.php
@@ -21,9 +21,35 @@ class DatabaseSeeder extends Seeder
          //   'email' => 'email@example.com',
        // ]);
 
+        User::create([
+            'username' => 'bcauser',
+            'password' => Hash::make('123456'),
+            'saldo' => 1000000
+        ]);
 
+        User::create([
+            'username' => 'erie',
+            'password' => Hash::make('rahasia'),
+            'saldo' => 100000000
+        ]);
 
+        User::create([
+            'username' => 'patricia',
+            'password' => Hash::make('rahasia'),
+            'saldo' => 100000
+        ]);
 
+        User::create([
+            'username' => 'natasya',
+            'password' => Hash::make('rahasia'),
+            'saldo' => 900000
+        ]);
+
+        User::create([
+            'username' => 'livia',
+            'password' => Hash::make('rahasia'),
+            'saldo' => 40000
+        ]);
 
     }
 }
