2f03ea42dc83431e80f255ca3726060952122380
vickyerie <vickyerie.s@gmail.com>
2025-06-11 20:35:56 +0700

    membuat route untuk fitur yang akan dibuat

--- klikbca-clone/routes/web.php
+++ klikbca-clone/routes/web.php
@@ -1,1 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'authenticate']);
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/saldo', [DashboardController::class, 'saldo']);
+    Route::get('/transfer', [DashboardController::class, 'transfer']);
+    Route::post('/transfer', [DashboardController::class, 'processTransfer']);
+    Route::get('/pembayaran', [DashboardController::class, 'pembayaran'])->middleware('auth');
+    Route::post('/pembayaran', [DashboardController::class, 'processPembayaran'])->middleware('auth');
+    Route::get('/mutasi', [DashboardController::class, 'mutasi']);
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+});
 
