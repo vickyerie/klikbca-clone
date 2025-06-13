a491475423c3174438a8e015dc1d3155c6420adf
vickyerie <vickyerie.s@gmail.com>
2025-06-13 19:34:46 +0700

    ngelarin fitur payment

--- klikbca-clone/app/Models/Payment.php
+++ klikbca-clone/app/Models/Payment.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
+    use HasFactory;
 
+    protected $fillable = ['user_id', 'kategori', 'keterangan', 'amount'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
