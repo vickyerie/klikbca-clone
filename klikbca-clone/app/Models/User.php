a2dc646847870f3bff1a8c6fc310aede747cef41
vickyerie <vickyerie.s@gmail.com>
2025-06-13 19:38:43 +0700

    nambahin function untuk fitur payment + user model attributes

--- klikbca-clone/app/Models/User.php
+++ klikbca-clone/app/Models/User.php
@@ -9,9 +9,26 @@ use App\Models\Transaction;
 
 class User extends Authenticatable
 {
+    use HasFactory, Notifiable;
+
+    protected $fillable = ['username', 'password', 'saldo'];
+
+    protected $hidden = ['password', 'remember_token'];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
     public function transactions()
     {
         return $this->hasMany(Transaction::class, 'user_id');
     }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+
 }
 
